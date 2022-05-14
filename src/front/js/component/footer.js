5b2018dab9841b7bcb20b3dbd37cf28dfa474c63
Luciano-C <luciano.cabrales@gmail.com>
2022-05-14 01:50:13 +0000

    footer con logos

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,10 +1,16 @@
 import React, { Component } from "react";
+import logo from "../../img/logo.png";
+import team from "../../img/team-toad.png";
+import "../../styles/footer.css";
 
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
-	</footer>
+  <footer className="footer mt-auto py-3">
+    <div className="d-flex justify-content-between">
+      <img className="appLogo" src={logo} />
+      <div className="logoContainer d-flex justify-content-center">
+        <span>Powered by:</span>
+        <img className="teamLogo" src={team} />
+      </div>
+    </div>
+  </footer>
 );
