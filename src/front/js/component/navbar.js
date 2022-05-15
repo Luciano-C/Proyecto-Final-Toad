93258d8f65aa9f2080548874e9378ed5f7c285d2
EvadneO <evadne28@gmail.com>
2022-05-15 02:57:08 +0000

    parte evadne

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,19 +1,42 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo from "../../img/logo.png";
+import "../../styles/home.css";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+  return (
+    <div className="container">
+      <nav className="navbar navbar bg-light mb-3">
+        <div className="logo">
+          <Link to="/">
+            <img src={logo} className="imagen"></img>
+          </Link>
+        </div>
+        <div className="col">
+          <Link to="/">
+            <button className="btn btn-ligth" type="button">
+              ADOPTA
+            </button>
+          </Link>
+          <Link to="/">
+            <button className="btn btn-ligth" type="button">
+              REQUISITOS DE ADOPCIÓN
+            </button>
+          </Link>
+          <Link to="/">
+            <button className="btn btn-ligth" type="button">
+              COMO ADOPTAR
+            </button>
+          </Link>
+        </div>
+        <div className="ml-auto">
+          <Link to="/demo">
+            <button className="btn btn-dark">
+              <i class="fas fa-user"> Inicia sección </i>
+            </button>
+          </Link>
+        </div>
+      </nav>
+    </div>
+  );
 };
