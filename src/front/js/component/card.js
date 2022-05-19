865f0232168a988b175635971ae1c23e84384bb7
EvadneO <evadne28@gmail.com>
2022-05-19 01:18:47 +0000

    Home actualizado

--- src/front/js/component/card.js
+++ src/front/js/component/card.js
@@ -1,23 +1,26 @@
 import React, { Component, useContext } from "react";
 import "../../styles/home.css";
+import { Link } from "react-router-dom";
 
-export const Card = () => {
+export const Card = (props) => {
   return (
-    <div class="card" style="width: 18rem;">
+    <div class="card" style={{ width: "18rem" }}>
       <img
         src="https://www.thesprucepets.com/thmb/bzdiSVEaF7gjQ_GKtKprxKnqOtE=/2124x1195/smart/filters:no_upscale()/GoldenPuppy185743593-56a9c1f23df78cf772aa4a33.jpg"
         class="card-img-top"
         alt="..."
       />
       <div class="card-body">
-        <h5 class="card-title">Card title</h5>
+        <h5 class="card-title">Mascota</h5>
         <p class="card-text">
           Some quick example text to build on the card title and make up the
           bulk of the card's content.
         </p>
-        <a href="#" class="btn btn-primary">
-          Go somewhere
-        </a>
+        <Link to="/demo">
+          <button className="cardButton btn btn-lg btn-danger outline-info">
+            Leer más
+          </button>
+        </Link>
       </div>
     </div>
   );
--- src/front/js/pages/demo.js
+++ src/front/js/pages/demo.js
@@ -4,38 +4,42 @@ import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
 
 export const Demo = () => {
-	const { store, actions } = useContext(Context);
-
-	return (
-		<div className="container">
-			<ul className="list-group">
-				{store.demo.map((item, index) => {
-					return (
-						<li
-							key={index}
-							className="list-group-item d-flex justify-content-between"
-							style={{ background: item.background }}>
-							<Link to={"/single/" + index}>
-								<span>Link to: {item.title}</span>
-							</Link>
-							{// Conditional render example
-							// Check to see if the background is orange, if so, display the message
-							item.background === "orange" ? (
-								<p style={{ color: item.initial }}>
-									Check store/flux.js scroll to the actions to see the code
-								</p>
-							) : null}
-							<button className="btn btn-success" onClick={() => actions.changeColor(index, "orange")}>
-								Change Color
-							</button>
-						</li>
-					);
-				})}
-			</ul>
-			<br />
-			<Link to="/">
-				<button className="btn btn-primary">Back home</button>
-			</Link>
-		</div>
-	);
+  const { store, actions } = useContext(Context);
+  return (
+    <div className="container">
+      <div className="card mb-5">
+        <img
+          src="https://p4.wallpaperbetter.com/wallpaper/524/805/269/cute-puppy-dog-pet-face-hand-wallpaper-preview.jpg"
+          className="card-img-top"
+          alt="..."
+        />
+        <div className="card-body">
+          <h1 className="card-title">
+            <strong>Tobby</strong>
+          </h1>
+          <h2 className="card-text">Raza</h2>
+          <p className="card-text">Edad:</p>
+          <p className="card-text">Ubicación:</p>
+          <p className="card-text">Descripción:</p>
+          <p className="card-text">
+            <small className="text-muted">
+              Publicado el 18 de Mayo del 2022.
+            </small>
+          </p>
+        </div>
+      </div>
+      <div className="d-grid gap-2 d-md-flex justify-content-md-end">
+        <Link to="/">
+          <button className="cardButton btn btn-lg btn-danger outline-info">
+            Regresar a Home
+          </button>
+        </Link>
+        <Link to="/">
+          <button className="cardButton btn btn-lg btn-danger outline-info">
+            Adoptar
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
 };
