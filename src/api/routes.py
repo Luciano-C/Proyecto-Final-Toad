3bca24e1af7d1f8781eaf543e4524213d2ca9684
Luciano-C <luciano.cabrales@gmail.com>
2022-05-19 01:10:02 +0000

    GET mascotas

--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Mascota, Usuario_Mascota
 from api.utils import generate_sitemap, APIException
 
 api = Blueprint('api', __name__)
@@ -16,3 +16,9 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route("/mascotas", methods=["GET"])
+def get_mascotas():
+    todas_mascotas = Mascota.query.all()
+    todas_mascotas = list(map(lambda x: x.serialize(), todas_mascotas))
+    return jsonify(todas_mascotas), 200
