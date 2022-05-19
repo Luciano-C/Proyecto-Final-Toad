23a412fa451049c1cdc9282beebec784b600edbf
Luciano-C <luciano.cabrales@gmail.com>
2022-05-19 21:40:58 +0000

    Añadida ruta para dueño. Revisar

--- src/api/routes.py
+++ src/api/routes.py
@@ -17,14 +17,21 @@ def handle_hello():
 
     return jsonify(response_body), 200
 
+@api.route("/usuarios", methods=["GET"])
+def get_usuarios():
+    todos_usuarios = User.query.all()
+    todos_usuarios = list(map(lambda x: x.serialize(), todos_usuarios))
+    return jsonify(todos_usuarios), 200
+
 @api.route("/mascotas", methods=["GET"])
 def get_mascotas():
     todas_mascotas = Mascota.query.all()
     todas_mascotas = list(map(lambda x: x.serialize(), todas_mascotas))
     return jsonify(todas_mascotas), 200
 
-@api.route("/user/<int:user_id>/crear-mascota", methods=["POST"])
-def crear_mascota(user_id):
+
+@api.route("/crear-mascota", methods=["POST"])
+def crear_mascota():
     data = request.get_json()
     nueva_mascota = Mascota()
     #perro_1 = {"nombre": "Rex", "edad": 5, "especie": "perro", "sexo": "Macho", "tamaño": "Mediano", "nivel_actividad": "Alto", "otros_cuidados": "Ninguno", "url_foto": "https://res.cloudinary.com/dnwy0nzzr/image/upload/v1652914840/qxe66pkjkgrfu6z7f6t2.jpg"}
@@ -40,5 +47,16 @@ def crear_mascota(user_id):
     db.session.add(nueva_mascota)
     db.session.commit()
 
-    return f"Mascota añadida para usuario {user_id}"
+    return f"Mascota añadida"
 
+@api.route("/añadir-usuario-mascota/id_usuario=<int:id_usuario>/id_mascota=<int:id_mascota>", methods=["POST"])
+def añadir_usuario_mascota(id_usuario, id_mascota):
+    #data = request.get_json()
+    nuevo_usuario_mascota = Usuario_Mascota()
+    nuevo_usuario_mascota.id_usuario = id_usuario
+    nuevo_usuario_mascota.id_mascota = id_mascota
+
+    db.session.add(nuevo_usuario_mascota)
+    db.session.commit()
+
+    return "Dueño añadido"
