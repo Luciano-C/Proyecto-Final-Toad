42744c2b2646688967b46ff59088b47c4f8d6dce
Luciano-C <luciano.cabrales@gmail.com>
2022-06-02 20:51:08 +0000

    Se arreglo problema con linea en VerMisPostulaciones cuando se demoraba en cargar fetch

--- src/front/js/pages/verMisPostulaciones.js
+++ src/front/js/pages/verMisPostulaciones.js
@@ -14,11 +14,13 @@ export const VerMisPostulaciones = () => {
   - Además se genera una lista con id de formularios.
   - Para cada elemento de la lista de objetos mascotas se crea un objeto temporal con el formato {mascota: {}, dueño: {}, idFormulario: #} y se hace un push a una lista vacía.
   - Al completar la iteración se fija la variable de estado arrayToMap y se mapea en jsx.
+
+  NOTA: Se añadió renderizado condicional y variable isLoading porque la línea "let idDueño = store.usuariosMascotas.filter((y) => y.id_mascota === x)[0].id_usuario;"
+  Estaba causando problemas cuando se demoraba en cargar el fetch.
   */
 
   const { store, actions } = useContext(Context);
-  const [candidatosMascotasFormularios, setCandidatosMascotasFormularios] =
-    useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   const [arrayToMap, setArrayToMap] = useState([
     { mascota: {}, dueño: {}, idFormulario: "" },
   ]);
@@ -35,47 +37,51 @@ export const VerMisPostulaciones = () => {
   }, [store.usuariosMascotas, store.usuarios, store.mascotas]);
 
   const generateArrayToMap = () => {
-    let arrayToMap = [];
-    const usuarioMascotasFormularioFiltrado =
-      store.usuariosMascotasFormularios.filter(
-        (x) => x.id_usuario === store.usuarioActual.id
+    try {
+      let arrayToMap = [];
+      const usuarioMascotasFormularioFiltrado =
+        store.usuariosMascotasFormularios.filter(
+          (x) => x.id_usuario === store.usuarioActual.id
+        );
+
+      const idMascotas = usuarioMascotasFormularioFiltrado.map(
+        (x) => x.id_mascota
+      );
+      const idFormularios = usuarioMascotasFormularioFiltrado.map(
+        (x) => x.id_formulario
       );
 
-    const idMascotas = usuarioMascotasFormularioFiltrado.map(
-      (x) => x.id_mascota
-    );
-    const idFormularios = usuarioMascotasFormularioFiltrado.map(
-      (x) => x.id_formulario
-    );
+      const idDueños = [];
+      idMascotas.forEach((x) => {
+        let idDueño = store.usuariosMascotas.filter(
+          (y) => y.id_mascota === x
+        )[0].id_usuario;
+        idDueños.push(idDueño);
+      });
 
-    const idDueños = [];
-    idMascotas.forEach((x) => {
-      let idDueño = store.usuariosMascotas.filter((y) => y.id_mascota === x)[0]
-        .id_usuario;
-      idDueños.push(idDueño);
-    });
+      const objetosMascotas = [];
+      idMascotas.forEach((x) => {
+        let mascota = store.mascotas.filter((y) => y.id === x);
+        objetosMascotas.push(...mascota);
+      });
 
-    const objetosMascotas = [];
-    idMascotas.forEach((x) => {
-      let mascota = store.mascotas.filter((y) => y.id === x);
-      objetosMascotas.push(...mascota);
-    });
+      const objetosDueños = [];
+      idDueños.forEach((x) => {
+        let dueño = store.usuarios.filter((y) => y.id === x);
+        objetosDueños.push(...dueño);
+      });
 
-    const objetosDueños = [];
-    idDueños.forEach((x) => {
-      let dueño = store.usuarios.filter((y) => y.id === x);
-      objetosDueños.push(...dueño);
-    });
-
-    objetosMascotas.forEach((x, i) => {
-      let infoToPush = {
-        mascota: x,
-        dueño: objetosDueños[i],
-        idFormulario: idFormularios[i],
-      };
-      arrayToMap.push(infoToPush);
-    });
-    setArrayToMap(arrayToMap);
+      objetosMascotas.forEach((x, i) => {
+        let infoToPush = {
+          mascota: x,
+          dueño: objetosDueños[i],
+          idFormulario: idFormularios[i],
+        };
+        arrayToMap.push(infoToPush);
+      });
+      setArrayToMap(arrayToMap);
+      setIsLoading(false);
+    } catch {}
   };
 
   /*   let arrayToMap = [
@@ -93,31 +99,41 @@ export const VerMisPostulaciones = () => {
 
   // candidatoMAscotasFormularios: [{ id: 15, id_formulario: 1, id_mascota: 11, id_usuario: 3 }, ...]
 
-  return (
-    <div className="container">
-      <table className="table text-center">
-        <thead>
-          <tr>
-            <th scope="col">Mascota</th>
-            <th scope="col">Dueño</th>
-            <th scope="col">Formulario</th>
-          </tr>
-        </thead>
-        <tbody>
-          {arrayToMap.map((x, i) => (
-            <tr key={`tr${i}`}>
-              <td key={`n${i}`}>{x.mascota.nombre}</td>
-              <td key={`c${i}`}>{`${x.dueño.nombre} ${x.dueño.apellidos}`}</td>
-
-              <td key={`l${i}`}>
-                <Link to={`/respuestas-candidato/${x.idFormulario}`}>
-                  Ver Respuestas
-                </Link>
-              </td>
+  if (!isLoading) {
+    return (
+      <div className="container">
+        <table className="table text-center">
+          <thead>
+            <tr>
+              <th scope="col">Mascota</th>
+              <th scope="col">Dueño</th>
+              <th scope="col">Formulario</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
+          </thead>
+          <tbody>
+            {arrayToMap.map((x, i) => (
+              <tr key={`tr${i}`}>
+                <td key={`n${i}`}>{x.mascota.nombre}</td>
+                <td
+                  key={`c${i}`}
+                >{`${x.dueño.nombre} ${x.dueño.apellidos}`}</td>
+
+                <td key={`l${i}`}>
+                  <Link to={`/respuestas-candidato/${x.idFormulario}`}>
+                    Ver Respuestas
+                  </Link>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  } else {
+    return (
+      <div className="spinner-grow text-primary" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </div>
+    );
+  }
 };
