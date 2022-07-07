ceb941d2948af65e3465a0632a6a7cb8923993fc
EvadneO <evadne28@gmail.com>
2022-07-07 14:37:18 +0000

    cambios

--- migrations/versions/c39d5f51bff8_.py
+++ migrations/versions/c39d5f51bff8_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 81fc56afdf8c
+Revision ID: c39d5f51bff8
 Revises: 
-Create Date: 2022-06-13 23:57:56.498171
+Create Date: 2022-07-06 18:57:22.848979
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '81fc56afdf8c'
+revision = 'c39d5f51bff8'
 down_revision = None
 branch_labels = None
 depends_on = None
